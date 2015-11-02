55b2de8b27f7503b35e7e197c0435fe0796c17e1
Farewe <leroy.boris@gmail.com>
2015-11-02 15:47:47 +0100

    Created code to start profiling:  - initialisation file  - slow but working code

---
+++ scripts/initialisation.R
@@ -0,0 +1,36 @@
+# Initialisation script
+library(virtualspecies)
+library(reshape2)
+library(ggplot2)
+library(rworldmap)
+source("./scripts/functions/patch_generation.R")
+worldclim <- getData("worldclim", var = "bio", res = 10)
+worldclim <- worldclim[["bio1"]]
+
+# Downloading worldclim data
+worldclim <- getData("worldclim", var = "bio", res = 10)
+worldclim <- worldclim[["bio1"]]
+
+# Resampling to the resolution used in the paper
+worldclim <- aggregate(worldclim, fact = 3)
+
+# We will crop to North America
+worldmap<- getMap()
+northam <- extent(-180, -15, 5, 85)
+bio1 <- crop(worldclim, northam)
+bio1 <- mask(bio1, worldmap[which(worldmap@data$SUBUNIT %in% 
+                                    c("United States of America", "Canada", "Greenland", 
+                                      "Mexico", "Panama", "Costa Rica", "Guatemala", 
+                                      "Honduras", "Nicaragua")), ])
+
+rm(worldclim)
+
+temperature.gradient <- seq(worldclim[["bio1"]]@data@min - 100,
+                            worldclim[["bio1"]]@data@max, 
+                            length = 1000)
+
+gauss.resp <- function(x., mean., diff., prob.)
+{
+  custnorm(x = x., mean = mean., diff = diff., prob = prob.) /
+    custnorm(x = mean., mean = mean., diff = diff., prob = prob.)
+}
